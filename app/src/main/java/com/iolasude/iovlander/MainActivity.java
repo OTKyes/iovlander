488535598ac98733e64a207119cb488dfbf5876b
OTKyes <otkyes@outlook.com>
2021-09-28 01:12:14 +0800

    Game Loop

--- app/src/main/java/com/iolasude/iovlander/MainActivity.java
+++ app/src/main/java/com/iolasude/iovlander/MainActivity.java
@@ -3,12 +3,31 @@ package com.iolasude.iovlander;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //srt content view to game
+
+        setContentView(new  Game(this));
+
+        //set window to full screen
+        if (getSupportActionBar() != null){
+            getSupportActionBar().hide();
+        }
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+
+
     }
 }
